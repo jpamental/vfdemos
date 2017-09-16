e7bb09cc625382b80ea890aa61486430f03e8404
Jason Pamental <jpamental@isovera.com>
2017-09-16 16:41:51 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# vfdemos
+# Intentional Typography & Variable Font Demos
 Typographic system demonstration pages, in particular showcasing the use of variable fonts from a range of different designers.
---
+++ js/main.js
@@ -0,0 +1,21 @@
+/* Widowtamer */
+wt.fix({
+	elements: 'p',
+	chars: 15,
+	method: 'nbsp',
+	event: 'resize'
+});
+
+wt.fix({
+	elements: 'blockquote',
+	chars: 2,
+	method: 'nbsp',
+	event: 'resize'
+});
+
+wt.fix({
+	elements: 'h1',
+	chars: 6,
+	method: 'nbsp',
+	event: 'resize'
+});
